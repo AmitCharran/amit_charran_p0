f430949423b6554a0088546294bd0c6e22a7d016
Amit <amit.charran@gmail.com>
2021-07-30 17:03:50 -0400

    Adding repository for my project

---
+++ src/main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+
+
+    }
+}
