051c65c029f42ef8230117514ca3e6d55089d05a
Amit <amit.charran@gmail.com>
2021-08-01 17:50:30 -0400

    updated my planning.txt file

--- amit_charran_p0/src/main.java
+++ amit_charran_p0/src/main.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class main {
     public static void main(String[] args) {
-        List<String> myList = new ArrayList<>();
-        ArrayList<String> list2 = new ArrayList<>();
+        System.out.println("Hello");
 
 
     }
